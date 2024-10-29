ac44e9f26df20cecee2abefa3934754a8437dd75
Sharon Amelinda <sharonamelindaa@gmail.com>
2024-10-29 12:00:52 +0700

    Updated

---
+++ src/js/swipe.js
@@ -0,0 +1,21 @@
+const swiperfood = new Swiper('.swiper-food', {
+    direction: 'horizontal',
+    spaceBetween: 20,
+    slidesOffsetAfter: 20,
+    slidesOffsetBefore: 20,
+    freeMode: true
+})
+
+const swipercategory = new Swiper('.swiper-category', {
+    direction: 'horizontal',
+    spaceBetween: 20,
+    slidesOffsetAfter: 20,
+    slidesOffsetBefore: 20,
+    freeMode: true
+})
+
+const swipergallery = new Swiper('.swiper-food-gallery', {
+    direction: 'horizontal',
+    spaceBetween: 20,
+    freeMode: true
+})
